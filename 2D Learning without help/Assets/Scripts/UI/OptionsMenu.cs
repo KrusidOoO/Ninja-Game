c83bf0a2a7aaae3d3ae184729e4e28fb21a5e682
KrusidOoO <KrusidOoO@users.noreply.github.com>
2019-11-20 13:08:15 +0100

    Addition of several UI menus including pause menu, settings menu and so on

---
+++ 2D Learning without help/Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Dropdown resDropdown;
+
+    Resolution[] resolutions;
+    void Start()
+    {
+        resolutions=Screen.resolutions;
+
+        resDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i=0;i<resolutions.Length;i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width==Screen.currentResolution.width&&resolutions[i].height==Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resDropdown.AddOptions(options);
+        resDropdown.value = currentResolutionIndex;
+        resDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res=resolutions[resolutionIndex];
+        Screen.SetResolution(res.width,res.height,Screen.fullScreen);
+    }
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
